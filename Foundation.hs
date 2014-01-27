7cbec0df18161795215db47fa26322e0a8596dff
Christian Kjaer Laustsen <christianlaustsen@gmail.com>
2014-01-27 16:59:40 +0100

    Remove messages (i18n) since it wasn't used.

--- Foundation.hs
+++ Foundation.hs
@@ -38,7 +38,7 @@ data App = App
     }
 
 -- Set up i18n messages. See the message folder.
-mkMessage "App" "messages" "en"
+-- mkMessage "App" "messages" "en"
 
 -- This is where we define all of the routes in our application. For a full
 -- explanation of the syntax, please see:
